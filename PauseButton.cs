8d443ff139d97801ae8929a70e1d74c7e5e48822
kimjo3900 <kimjo3900@gmail.com>
2021-02-16 13:52:14 -0500

    Add files via upload
    Allows for two sprite objects (play/pause button) to be attached to this GameObject and switches between the two images when button is pressed

---
+++ PauseButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour
+{
+	private Button button;
+	public Sprite play, pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void ChangeImage() {
+		if (button.image.sprite == pause)
+			button.image.sprite = play;
+		else
+			button.image.sprite = pause;
+	}
+}
