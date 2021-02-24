15f4b3eb4d61a7334103f6e381e9e934b9daf64b
kimjo3900 <kimjo3900@gmail.com>
2021-02-24 11:29:22 -0500

    Continuous speed slider
    Speed slider now allows for smooth, continuous scrolling
    Multiplier Display now reads actual speed value instead of the speed slider setting to determine the appropriate value and unit to display

--- SpeedMultiDisplay.cs
+++ SpeedMultiDisplay.cs
@@ -1,26 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.UI;
 
 public class SpeedMultiDisplay : MonoBehaviour
 {
 	private Text txt;
-	private GameObject slider;
-	private float val;
-	private List<string> times = new List<string>() {"1 sec", "7 sec", "1 min", "5 min", "30 min", "4 hours", "1 day", "1 week", "2 months", "1 year"};
+	private GameObject date;
+	private int speed;
 
     // Start is called before the first frame update
     void Start()
     {
         txt = GetComponent<Text>();
-		slider = transform.parent.gameObject;
+		date = GameObject.Find("Date & Time");
     }
 
     // Update is called once per frame
     void Update()
     {
-		val = slider.GetComponent<Slider>().value;
-        txt.text = "1 sec = " + times[(int)val];
+		DateTimeDisplay dateObj = date.GetComponent("DateTimeDisplay") as DateTimeDisplay;
+		speed = (int)Math.Abs(dateObj.speed);
+		
+		txt.text = "1 sec = ";
+		if (speed < 60) 
+			txt.text += speed + " sec";
+		else if (speed < 3600)
+			txt.text += speed/60 + " min";
+		else if (speed < 7200)
+			txt.text += speed/3600 + " hour";
+		else if (speed < 86400)
+			txt.text += speed/3600 + " hours";
+		else if (speed < 172800)
+			txt.text += speed/86400 + " day";
+		else if (speed < 604800)
+			txt.text += speed/86400 + " days";
+		else if (speed < 1209600)
+			txt.text += speed/604800 + " week";
+		else if (speed < 2629728)
+			txt.text += speed/604800 + " weeks";
+		else if (speed < 5259456)
+			txt.text += speed/2629728 + " month";
+		else if (speed < 31500000)
+			txt.text += speed/2629728 + " months";
+		else
+			txt.text += speed/31500000 + " year";
     }
 }
